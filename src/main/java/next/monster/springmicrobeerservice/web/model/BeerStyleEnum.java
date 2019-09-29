e5e75858cb8456a77acd0225b3279e7e53f6b6c9
Anil Mirge <manilche@gmail.com>
2019-09-29 16:36:29 -0400

    Adding Web Data Model.

---
+++ src/main/java/next/monster/springmicrobeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package next.monster.springmicrobeerservice.web.model;
+
+public enum BeerStyleEnum {
+  LAGER,
+  PILSNER,
+  STOUT,
+  GOSE,
+  PORTER,
+  ALE,
+  WHEAT,
+  IPA,
+  PALE_ALE,
+  SAISON
+}
