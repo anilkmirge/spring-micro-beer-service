868bc2fd2faeded075ecbe6758e2e5b96b09cd9f
amirge <anil.mirge@monster.com>
2019-11-05 12:16:26 -0500

    Adding Beer Entity. Closes #5

---
+++ src/main/java/next/monster/springmicrobeerservice/domain/Beer.java
@@ -0,0 +1,54 @@
+package next.monster.springmicrobeerservice.domain;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+  @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+  private UUID id;
+
+  @Version
+  private Long version;
+
+  private String beerName;
+  private String beerStyle;
+
+  @Column(unique = true)
+  private Long upc;
+  private BigDecimal price;
+
+  @CreationTimestamp
+  @Column(updatable = false)
+  private Timestamp createdDate;
+
+  @UpdateTimestamp
+  private Timestamp lastModifiedDate;
+
+  private Integer minOnHand;
+  private Integer quantityToBrew;
+
+}
