abfd6a78a53616e26e58a1ee616166cc33e4b3b6
amirge <anil.mirge@monster.com>
2019-11-14 15:55:13 -0500

    Added validation and error handler to controller. This closes #4.

--- src/main/java/next/monster/springmicrobeerservice/web/controller/BeerController.java
+++ src/main/java/next/monster/springmicrobeerservice/web/controller/BeerController.java
@@ -3,28 +3,30 @@ package next.monster.springmicrobeerservice.web.controller;
 import next.monster.springmicrobeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/beer")
+@RequestMapping("/api/v1/")
 public class BeerController {
 
-  @GetMapping("/{beerId}")
+  @GetMapping("beer/{beerId}")
   public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
     //todo impl
     return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
   }
 
-  @PostMapping()
-  public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beer) {
+  @PostMapping(path = "beer")
+  public ResponseEntity<BeerDto> saveNewBeer(@Validated @RequestBody BeerDto beer) {
     //todo impl
     return new ResponseEntity<>(HttpStatus.CREATED);
   }
 
-  @PutMapping("/{beerId}")
-  public ResponseEntity<BeerDto> updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beer) {
+  @PutMapping("beer/{beerId}")
+  public ResponseEntity<BeerDto> updateBeerById(@PathVariable("beerId") UUID beerId,
+      @Validated @RequestBody BeerDto beer) {
     //todo impl
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
---
+++ src/main/java/next/monster/springmicrobeerservice/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,23 @@
+package next.monster.springmicrobeerservice.web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<List> validationErrorHandler(ConstraintViolationException ex) {
+    List<String> errors = new ArrayList<>(ex.getConstraintViolations().size());
+
+    ex.getConstraintViolations().forEach(error -> errors.add(error.toString()));
+
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
+
+}
