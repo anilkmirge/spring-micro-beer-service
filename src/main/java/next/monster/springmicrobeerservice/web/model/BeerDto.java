298470bf50634118bca69c349c618484f89f85ae
amirge <anil.mirge@monster.com>
2019-11-14 13:43:49 -0500

    Added Bean Validation. This closes # 3.

--- src/main/java/next/monster/springmicrobeerservice/web/model/BeerDto.java
+++ src/main/java/next/monster/springmicrobeerservice/web/model/BeerDto.java
@@ -1,5 +1,9 @@
 package next.monster.springmicrobeerservice.web.model;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,16 +18,33 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+
+  @Null
   private UUID id;
+
+  @Null
   private Integer version;
 
+  @Null
   private OffsetDateTime createdDate;
+
+  @Null
   private OffsetDateTime lastModifiedDate;
 
+  @NotEmpty
   private String beerName;
+
+  @NotNull
   private BeerStyleEnum beerStyle;
+
+  @NotNull
+  @Positive
   private String upc;
+
+  @NotNull
+  @Positive
   private BigDecimal price;
+
   private Integer quantityOnHand;
 
 }
